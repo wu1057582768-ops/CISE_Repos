7cbc0f23c3358e4f21838a256cd11436de0032ae
wu1057582768-ops <1057582768@qq.com>
2025-09-10 09:21:51 +0800

    3

--- worksheet1/aut-api/src/app.controller.spec.ts
+++ worksheet1/aut-api/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "worksheet1"', () => {
+      expect(appController.getHello()).toBe('worksheet1');
     });
   });
 });
