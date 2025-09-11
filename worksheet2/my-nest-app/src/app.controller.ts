a465772a83ee11dfb79851b53c745ef7e2ce843c
wu1057582768-ops <1057582768@qq.com>
2025-09-11 09:12:15 +0800

    worksheet2

--- worksheet2/my-nest-app/src/app.controller.ts
+++ worksheet2/my-nest-app/src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { ARTICLES } from './dummydata/articles';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+
+// 假设文章数据结构如下，根据实际数据结构调整
+interface Article {
+  _id: string | number;
+  title: string;
+  content: string;
+  // 其他字段...
+}
 
 @Controller()
 export class AppController {
@@ -8,5 +17,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/api/articles')
+  getArticles(): Article[] {
+    return ARTICLES as Article[];
+  }
+
+  @Get('/api/articles/:id')
+  getArticleById(@Param('id') id: string): Article | undefined {
+    return (ARTICLES as Article[]).find((n: Article) => n._id === id);
   }
 }
